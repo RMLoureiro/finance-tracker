c5720a19602d4a512c5803a275c098a5f51e91e0
Ricardo Loureiro <rfmloureiro@gmail.com>
2025-07-12 03:50:53 +0100

    [FT-11] add transactions (#14)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_12_015451) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_12_023517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,6 +30,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_12_015451) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.float "ammount"
+    t.bigint "account_id", null: false
+    t.bigint "transaction_type_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_transactions_on_account_id"
+    t.index ["transaction_type_id"], name: "index_transactions_on_transaction_type_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "name"
@@ -40,4 +50,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_12_015451) do
   end
 
   add_foreign_key "accounts", "users"
+  add_foreign_key "transactions", "accounts"
+  add_foreign_key "transactions", "transaction_types"
 end
